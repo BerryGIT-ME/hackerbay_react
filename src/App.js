bcdf91678920ad580f929588ca7ea0d15a8c8290
Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
2021-07-07 00:44:02 +0100

    added a button to start the game

--- src/App.js
+++ src/App.js
@@ -4,28 +4,52 @@ import { GreenSpriteProvider } from "./context/GreenSpriteContext";
 import { CenterContextProvider } from "./context/CenterContext";
 import { GreenSpriteNoContextProvider } from "./context/GreenSpriteNoContext";
 import { CountContextProvider } from "./context/CountContext";
+import { useState } from "react";
 import Column from "./components/Column";
 import "./app.css";
 
 function App() {
+  const [play, setplay] = useState(false);
+
+  const handleStartGameClick = () => {
+    setplay(true);
+  };
   return (
     <div className="container">
-      <h3>
-        Click the red button and move around with the direction keys until all
-        the green buttons are cleared
-      </h3>
-      <BoardSizeProvider>
-        <GreenSpriteProvider>
-          <CenterContextProvider>
-            <GreenSpriteNoContextProvider>
-              <CountContextProvider>
-                <BoardSize />
-                <Column />
-              </CountContextProvider>
-            </GreenSpriteNoContextProvider>
-          </CenterContextProvider>
-        </GreenSpriteProvider>
-      </BoardSizeProvider>
+      {!play && (
+        <h4>
+          Instructions: Click the start game button and define the size of the
+          game board. Typical values are - height: 10 and width: 10
+        </h4>
+      )}
+      {play && (
+        <h4>
+          Click the red button and move around with the direction keys (arrow
+          keys on the keyboard) until all the green buttons are cleared
+        </h4>
+      )}
+
+      <button
+        type="button"
+        style={{ margin: 20 }}
+        onClick={() => handleStartGameClick()}
+      >
+        Start game
+      </button>
+      {play && (
+        <BoardSizeProvider>
+          <GreenSpriteProvider>
+            <CenterContextProvider>
+              <GreenSpriteNoContextProvider>
+                <CountContextProvider>
+                  <BoardSize />
+                  <Column />
+                </CountContextProvider>
+              </GreenSpriteNoContextProvider>
+            </CenterContextProvider>
+          </GreenSpriteProvider>
+        </BoardSizeProvider>
+      )}
     </div>
   );
 }
